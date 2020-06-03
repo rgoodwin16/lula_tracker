7489c36fcf0280030c31335e91ec7623c44ca544
Lula Goodwin <33987325+TheLulaGoodwin@users.noreply.github.com>
2020-06-03 16:19:01 -0400

    add user repo and db set

--- LulaTracker/LulaTracker.Data/DataContext.cs
+++ LulaTracker/LulaTracker.Data/DataContext.cs
@@ -13,5 +13,7 @@ namespace LulaTracker.Data
         public DbSet<Project> Projects { get; set; }
 
         public DbSet<Ticket> Tickets { get; set; }
+
+        public DbSet<User> Users { get; set; }
     }
 }
---
+++ LulaTracker/LulaTracker.Data/Repositories/UserRepository.cs
@@ -0,0 +1,20 @@
+using System.Linq;
+using LulaTracker.Core.Entities;
+using LulaTracker.Core.Interfaces.Repositories;
+
+namespace LulaTracker.Data.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataContext _context;
+
+        public UserRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public User GetUserById(int id)
+        {
+            return _context.Users.FirstOrDefault(t => t.Id == id);
+        }
+    }
+}
